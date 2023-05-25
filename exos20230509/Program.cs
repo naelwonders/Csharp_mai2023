b24d639362622a2df709c1adf8ef6dcd6c70cc01
Nael Lammens <nslammens@gmail.com>
2023-05-25 14:08:02 +0200

    Add project files.

---
+++ exos20230509/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+
+namespace exos20230509
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // pour déplacer un bout de code en meme temps, tu selectionnes et alt et fleches
+            // pour mettre en commentaire: ctl  + K + C (ou U pour le contraire)
+            #region Tableau (meme type et taille fixe)
+
+            #region Tableau 1D
+
+            //int[] tableau = { 1, 2, 3, 4, 5, 6, 7 };
+            //for (int i = 0; i < tableau.Length; i++)
+            //{
+            //    Console.WriteLine(tableau[i]);
+            //}
+            #endregion
+
+            #region Tableau 2D
+
+
+            //int[,] tableau2 = { { 1, 2, 3 }, 
+            //                    { 4, 5, 6 }, 
+            //                    { 7, 8, 9 } };
+            //for (int i = 0; i < tableau2.GetLength(0); i++) // 0 c'est ligne et 1 c'est colonnes
+            //{
+            //    for ( int j = 0; j < tableau2.GetLength(1); j++)
+            //    {
+            //        Console.WriteLine(tableau2[i,j]);
+            //    }
+            //}
+            #endregion
+
+            #region Tableau 3D
+
+            // deux virgules pour un tableau en 3D (genre rubiscube)
+            //int[,,] tableau3 = {
+            //                    { { 0, 1, 0 }, { 0, 3, 0 }, { 0, 3, 0 } },
+            //                    { { 0, 4, 0 }, { 0, 5, 0 }, { 0, 6, 0 } },
+            //                    { { 0, 7, 0 }, { 0, 8, 0 }, { 0, 9, 0 } }
+            //                    };
+
+            //for (int i = 0; i < tableau3.GetLength(0); i++) // 0 c'est ligne et 1 c'est colonnes
+            //{
+            //    for (int j = 0; j < tableau3.GetLength(1); j++)
+            //    {
+            //        for (int k = 0; k < tableau3.GetLength(2); k++)
+            //        {
+            //            Console.WriteLine(tableau3[i, j, k]);
+            //        };
+            //    }
+            //}
+            #endregion
+
+            #endregion
+
+            #region Collection (taille variable et typage different)
+
+            ArrayList arrayList = new ArrayList(); // obliger de l'instancier avec un new
+
+            // pour copier coller une ligne rapidement juste en dessous: tu te met à la fin de la ligne puis ctrl c et ctrl v et tac
+            // selectionne une valeur + ctrl H -> pour les changer une à la fois (et ca te donne le nombre d'occurances aussi)
+            // alt et tu clique ta valeur pour les selectionner toutes
+            arrayList.Add(3);
+            arrayList.Add("lala");
+
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine($"L'element \"{item}\" est : {item.GetType()}");
+            }
+            
+            #endregion
+
+        }
+    }
+}
---
+++ exos_20230509_tableaux/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Text;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace exos_20230509_tableaux
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Exo 1 
+            //Écrire un algorithme qui saisit 6 entiers et les stocke dans un tableau, puis affiche le contenu de ce tableau une fois qu’il est rempli.
+
+            //int[] tableau1 = new int[6];
+
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    int entier = 0;
+            //    bool success = false;
+            //    while (!success)
+            //    {
+            //        Console.WriteLine($"Donne un entier ({i + 1}/{tableau1.Length}) : ");
+            //        success = int.TryParse(Console.ReadLine(), out entier);
+
+            //    }
+
+            //    tableau1[i] = entier;
+
+            //}
+
+            //Console.Write($"Les elements du tableaux : ");
+
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    Console.Write($"{tableau1[i]} ");
+            //}
+
+            #endregion
+
+            #region Exo 2
+
+            // BONUS : initialiser un tableau de 10 entiers avec les valeurs 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024 à l’aide d’une boucle. Ensuite, à l’aide d’une boucle, afficher la valeur de chaque cellule du tableau avec l’opération Ecrire()
+
+            //int[] tableau2 = new int[10]; // {2, 4, 8, 16, 32, 64, 128, 256, 512, 1024}
+            //int number = 1;
+
+            //for (int i = 0; i < tableau2.Length; i++) 
+            //{
+            //    tableau2[i] = number *= 2;
+
+            //}
+
+            //foreach (int i in tableau2)
+            //{
+            //    Console.WriteLine(i + " ");
+            //}
+
+            #endregion
+
+            #region Exo 3
+
+            // Ecrire un algorithme demandant à l’utilisateur le nombre de joueurs (max 10 joueurs). Ensuite,l’algorithme doit demander à l’utilisateur le score de chaque joueur. Une fois ceci fini, il faut afficher la moyenne des scores.
+
+            //int joueurs = -1;
+            //bool is_int = false;
+
+            //while (joueurs < 0 || joueurs > 11)
+            //{
+            //    Console.WriteLine("Donnez le nombre de joueurs (max 10) : ");
+            //    is_int = int.TryParse(Console.ReadLine(), out joueurs);
+            //    Console.Clear();
+            //}
+            //// une liste de la taille du nombre de joueurs
+            //int[] scores = new int[joueurs];
+            //int score;
+            //int somme_scores = 0;
+
+            //for (int i = 0; i < scores.Length; i++)
+            //{
+            //    Console.WriteLine($"Donnez le score du joueur {i + 1} : ");
+            //    is_int = int.TryParse(Console.ReadLine(), out score);
+            //    Console.Clear();
+
+            //    if (is_int)
+            //    {
+            //        scores[i] = score;
+            //        somme_scores += scores[i];
+            //    }
+            //}
+
+            //int resultat = somme_scores / scores.Length;
+
+            //Console.WriteLine(resultat);
+            #endregion
+
+            #region Exo 4 (ICI pour la prochaine fois mais fait un new document)
+        }
+    }
+}
